7bb3cedd405c469ffbf5ca30c447620429c09f25
eric-ward <eric.ward@noaa.gov>
2016-10-14 11:37:13 -0700

    commit figure changes

--- figures/figure 3.r
+++ figures/figure 3.r
@@ -11,9 +11,10 @@ X = seq(1981,2005)
 subset = which(chnk$BroodYear%in%X)
 Y = log(as.numeric(chnk$RecPerSpawn[subset])) # log(R/S)
 colSeq = seq(150, 0, length.out = length(X))
+
 pdf("Figure 01 S-R relationships.pdf")
 par(mfrow = c(3,2), mgp=c(2,1,0), mai = c(0.5,0.5,0.3,0.1))
-plot(chnk$Escapement[subset], Y, col = rgb(colSeq,colSeq,colSeq, 255, maxColorValue=255), xlab="Spawners", ylab="log (R/S)", main = "Copper River Chinook", lwd=2, cex=1.6)
+plot(chnk$Escapement[subset], Y, col = rgb(colSeq,colSeq,colSeq, 255, maxColorValue=255), xlab="Spawners", ylab="log (R/S)", main = "Copper River Chinook", lwd=2, cex=1.7)
 
 # read in sockeye data
 coghill = read_excel("../data/salmon data/data for analysis/Coghill_Wild_Sockeye_final.xlsx")
@@ -24,24 +25,24 @@ subset = which(coghill$BroodYear%in%X)
 Y = log(as.numeric(coghill$RecPerSpawn[subset])) # log(R/S)
 colSeq = seq(150, 0, length.out = length(X))
 
-plot(coghill$Escapement[subset], Y, col = rgb(colSeq,colSeq,colSeq, 255, maxColorValue=255), xlab="Spawners", ylab="log (R/S)", main = "Coghill Lake sockeye", lwd=2, cex=1.6)
+plot(coghill$Escapement[subset], Y, col = rgb(colSeq,colSeq,colSeq, 255, maxColorValue=255), xlab="Spawners", ylab="log (R/S)", main = "Coghill Lake sockeye", lwd=2, cex=1.7)
 
 Y = log(as.numeric(eshamy$RecPerSpawn[subset])) # log(R/S)
-plot(eshamy$Escapement[subset], Y, col = rgb(colSeq,colSeq,colSeq, 255, maxColorValue=255), xlab="Spawners", ylab="log (R/S)", main = "Eshamy Lake sockeye", lwd=2, cex=1.6)
+plot(eshamy$Escapement[subset], Y, col = rgb(colSeq,colSeq,colSeq, 255, maxColorValue=255), xlab="Spawners", ylab="log (R/S)", main = "Eshamy Lake sockeye", lwd=2, cex=1.7)
 
 Y = log(as.numeric(copper$RecPerSpawn[subset])) # log(R/S)
-plot(copper$Escapement[subset], Y, col = rgb(colSeq,colSeq,colSeq, 255, maxColorValue=255), xlab="Spawners", ylab="log (R/S)", main = "Copper River sockeye", lwd=2, cex=1.6)
+plot(copper$Escapement[subset], Y, col = rgb(colSeq,colSeq,colSeq, 255, maxColorValue=255), xlab="Spawners", ylab="log (R/S)", main = "Copper River sockeye", lwd=2, cex=1.7)
 
 # Read in pink salmon data
 pink = read_excel("../data/salmon data/data for analysis/PWS_Wild_Pink_final.xlsx")
 subset = which(pink$BroodYear%in%seq(1981,2008))
 Y = log(as.numeric(pink$RecPerSpawn[subset])) # log(R/S)
-plot(pink$Escapement[subset], Y, col = rgb(colSeq,colSeq,colSeq, 255, maxColorValue=255), xlab="Spawners", ylab="log (R/S)", main = "PWS pink", lwd=2, cex=1.6)
+plot(pink$Escapement[subset], Y, col = rgb(colSeq,colSeq,colSeq, 255, maxColorValue=255), xlab="Spawners", ylab="log (R/S)", main = "PWS pink", lwd=2, cex=1.7)
 
 # Read in herring data
 herring = read_excel("../data/herring/PWS_herring_final.xlsx")
 subset = which(herring$BroodYear%in%seq(1981,2008))
 Y = log(as.numeric(herring$RecPerSpawn[subset])) # log(R/S)
-plot(herring$BroodYearSB[subset], Y, col = rgb(colSeq,colSeq,colSeq, 255, maxColorValue=255), xlab="Spawning biomass (mt)", ylab="log (age 3 recruits / SSB)", main = "PWS herring", cex=1.6, lwd=2)
+plot(herring$BroodYearSB[subset], Y, col = rgb(colSeq,colSeq,colSeq, 255, maxColorValue=255), xlab="Spawning biomass (mt)", ylab="log (age 3 recruits / SSB)", main = "PWS herring", cex=1.7, lwd=2)
 
 dev.off()
--- figures/figure 4.r
+++ figures/figure 4.r
@@ -13,8 +13,8 @@ Y = log(as.numeric(chnk$RecPerSpawn[subset])) # log(R/S)
 colSeq = seq(150, 0, length.out = length(X))
 
 par(mfrow = c(3,2), mgp=c(2,1,0), mai = c(0.5,0.5,0.3,0.1))
-plot(X, log(chnk$RecPerSpawn[subset]), col = "grey30", xlab="Spawners", ylab="log(R/S)", 
-  main = "Copper River Chinook", lwd=2, cex=1.2, type="b")
+plot(X, log(chnk$RecPerSpawn[subset]), col = "grey30", xlab="", ylab="log(R/S)",
+  main = "Copper River Chinook", lwd=2, cex=1.3, type="b")
 
 # read in sockeye data
 coghill = read_excel("../data/salmon data/data for analysis/Coghill_Wild_Sockeye_final.xlsx")
@@ -23,20 +23,20 @@ copper = read_excel("../data/salmon data/data for analysis/Copper_Wild_Sockeye_f
 X = seq(1981,2008)
 subset = which(coghill$BroodYear%in%X)
 
-plot(X, log(coghill$RecPerSpawn[subset]), col = "grey30", xlab="Spawners", ylab="log(R/S)", main = "Coghill Lake sockeye", lwd=2, cex=1.2, type="b")
+plot(X, log(coghill$RecPerSpawn[subset]), col = "grey30", xlab="", ylab="log(R/S)", main = "Coghill Lake sockeye", lwd=2, cex=1.3, type="b")
 subset = which(eshamy$BroodYear%in%X)
-plot(X, log(as.numeric(eshamy$RecPerSpawn[subset])), col = "grey30", xlab="Spawners", ylab="log(R/S)", main = "Eshamy Lake sockeye", lwd=2, cex=1.2, type="b")
+plot(X, log(as.numeric(eshamy$RecPerSpawn[subset])), col = "grey30", xlab="", ylab="log(R/S)", main = "Eshamy Lake sockeye", lwd=2, cex=1.3, type="b")
 subset = which(copper$BroodYear%in%X)
-plot(X, log(copper$RecPerSpawn[subset]), col = "grey30", xlab="Spawners", ylab="log(R/S)", main = "Copper River sockeye", lwd=2, cex=1.2, type="b")
+plot(X, log(copper$RecPerSpawn[subset]), col = "grey30", xlab="", ylab="log(R/S)", main = "Copper River sockeye", lwd=2, cex=1.3, type="b")
 
 # Read in pink salmon data
 pink = read_excel("../data/salmon data/data for analysis/PWS_Wild_Pink_final.xlsx")
 subset = which(pink$BroodYear%in%seq(1981,2008))
-plot(X, log(pink$RecPerSpawn[subset]), col = "grey30", xlab="Spawners", ylab="log(R/S)", main = "PWS pink", lwd=2, cex=1.2, type="b")
+plot(X, log(pink$RecPerSpawn[subset]), col = "grey30", xlab="", ylab="log(R/S)", main = "PWS pink", lwd=2, cex=1.3, type="b")
 
 # Read in herring data
 herring = read_excel("../data/herring/PWS_herring_final.xlsx")
 subset = which(herring$BroodYear%in%seq(1981,2008))
-plot(X, log(herring$RecPerSpawn[subset]), col="grey30", xlab="Spawning biomass (mt)", ylab="log (age 3 recruits / SSB)", main = "PWS herring", cex=1.2, lwd=2, type="b")
+plot(X, log(herring$RecPerSpawn[subset]), col="grey30", xlab="", ylab="log (age 3 recruits / SSB)", main = "PWS herring", cex=1.3, lwd=2, type="b")
 
 dev.off()
